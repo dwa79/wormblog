f09b8242015c79d8d03de0ea5989af58725594e7
Eddie <dwa79@sfu.ca>
2016-07-05 07:30:17 +0000

    add association and debug view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
     def create
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
             flash[:success] = "Article was successfully created!"
             redirect_to article_path(@article)
@@ -38,6 +39,7 @@ class ArticlesController < ApplicationController
     
     def update
        @article = Article.find(params[:id])
+       @article.user = User.first
        if @article.update(article_params)
            flash[:success] = "Article was successfully updated!"
            redirect_to article_path(@article)
