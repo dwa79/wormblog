a68920999f63b381e8041719a91432812272a77a
Eddie <dwa79@sfu.ca>
2016-07-09 22:23:58 +0000

    add signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get 'personal', to: 'pages#personal'
 
   resources :articles
+  
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
