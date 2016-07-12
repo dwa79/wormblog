6dcafdf8e4969256aecd7fea48769f5e17e22a11
Eddie <dwa79@sfu.ca>
2016-07-12 04:57:59 +0000

    bug fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     def create
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             flash[:success] = "Article was successfully created!"
             redirect_to article_path(@article)
@@ -43,7 +43,7 @@ class ArticlesController < ApplicationController
     
     def update
        @article = Article.find(params[:id])
-       @article.user = User.first
+       @article.user = current_user
        if @article.update(article_params)
            flash[:success] = "Article was successfully updated!"
            redirect_to article_path(@article)
