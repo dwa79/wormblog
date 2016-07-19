2018d7966c45c74c96ff059091edaa8799656f76
Eddie <dwa79@sfu.ca>
2016-07-19 20:37:12 +0000

    complete admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    has_many :articles, dependent: :destroy
+    has_many :articles, dependent: :destroy  #delete user will delete all the articles
     before_save { self.email = email.downcase }
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 30 }
     VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
