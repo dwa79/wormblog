205aef6c58e600fd54d0bee4c007faf65ae02271
Eddie <dwa79@sfu.ca>
2016-07-23 21:29:41 +0000

    add user avatar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723063555) do
+ActiveRecord::Schema.define(version: 20160723203138) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -54,9 +54,13 @@ ActiveRecord::Schema.define(version: 20160723063555) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
-    t.boolean  "admin",           default: false
-    t.boolean  "email_confirmed", default: false
+    t.boolean  "admin",               default: false
+    t.boolean  "email_confirmed",     default: false
     t.string   "confirm_token"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
