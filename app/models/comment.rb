2c582f57ee82afbb444ff11eaade49b7c8aec67b
Eddie <dwa79@sfu.ca>
2016-08-02 01:00:53 +0000

    add some validation to comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
     belongs_to :article
     #belongs_to :user
     validates :commenter, presence: true
-    validates :body, presence: true
+    validates :body, presence: true, length:{ minimum: 1, maximum: 500 }
     validates :article_id, presence: true
     #validates :user_id, presence: true
 end
