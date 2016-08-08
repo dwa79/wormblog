e34e1e18f2e0bcd8c7726c00dc273d8b86051778
Eddie <dwa79@sfu.ca>
2016-08-08 06:34:51 +0000

    fix admin edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,7 +43,7 @@ class ArticlesController < ApplicationController
     
     def update
        @article = Article.find(params[:id])
-       @article.user = current_user
+       #@article.user = current_user
        if @article.update(article_params)
            flash[:success] = "Article was successfully updated!"
            redirect_to article_path(@article)
